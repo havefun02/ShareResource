10e25dda598305db32596d35ecca8aa4a07a8ead
havefun02 <lapphan290802@gmail.com>
2024-11-05 15:34:30 +0700

    Add cal service and unit test

---
+++ ICalculate.cs
@@ -0,0 +1,14 @@
+﻿namespace ShareResource
+{
+    public interface ICalculate
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+    }
+
+    public class CalculateService : ICalculate
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+    }
+}
---
+++ Tests/CalculateTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace ShareResource.Tests
+{
+    [TestFixture]
+    public class CalculateTest
+    {
+        private  ICalculate _calculateService;
+        [SetUp]
+        public void Setup()
+        {
+            this._calculateService = new CalculateService();
+        }
+        [Test]
+        public void AddAssert()
+        {
+            int a=3; int b=4;
+            var result = _calculateService.Add(a, b);
+            Assert.That(result, Is.EqualTo(7));
+        }
+        [Test]
+        public void SubtractAsser()
+        {
+            int a = 3; int b = 4;
+            var result = _calculateService.Subtract(a, b);
+            Assert.That(result,Is.EqualTo(-1));
+        }
+
+    }
+}
